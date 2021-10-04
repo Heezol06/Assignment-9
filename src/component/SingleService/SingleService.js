e06514e8ef21cff9923ef8b8417c844156248f9e
Heezol06 <shahriarrahmanheezol@gmail.com>
2021-10-04 16:46:19 +0600

    third-commit

--- src/component/SingleService/SingleService.js
+++ src/component/SingleService/SingleService.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { Card, Col } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 
 const SingleService = (props) => {
     const {subject, about, price, img} =props.service
     return (
             <div className="col-md-6">
                 <Col className="my-4">
-                    <Card>
-                        <img src={img}className="h-75" alt="" srcset="" />
-                        <Card.Body>
-                        <Card.Title>{subject}</Card.Title>
-                        <h4>Price:{price}$</h4>
-                        <Card.Text>
-                            {about.slice(1,400)}
-                        </Card.Text>
-                        </Card.Body>
-                    </Card>
+                    <div class="card mb-3" style={{maxWidth: "540px"}}>
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src={img} class="img-fluid rounded-start" alt="..." />
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">{subject}</h5>
+        <h4>Price:{price}$</h4>
+        <p class="card-text">{about.slice(1,200)}</p>
+      </div>
+    </div>
+  </div>
+</div>
                  </Col>
                 </div>
     );
